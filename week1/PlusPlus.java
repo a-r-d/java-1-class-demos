3e82a546515d3973f0f8bacd0d4b28e5c06bc25f
Decker <aaron.decker@cincinnatistate.edu>
2016-08-31 21:30:13 -0400

    add class demos

---
+++ week1/PlusPlus.java
@@ -0,0 +1,17 @@
+package edu.cincinnatistate;
+
+public class PlusPlus {
+
+	public static void main(String[] args) {
+		
+		int i = 0;
+		
+		System.out.println(i++);
+		System.out.println(++i);
+		
+		int ii = 0;
+		System.out.println(--ii);
+
+	}
+
+}
---
+++ week1/RollingOver.java
@@ -0,0 +1,16 @@
+package edu.cincinnatistate;
+
+import java.math.BigInteger;
+
+public class RollingOver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte i = 127;
+		i++;
+		System.out.println(i);
+		
+		
+	}
+
+}
