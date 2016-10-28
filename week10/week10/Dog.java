3f5a5ede5313b3fc7efc1ce0308ad055879960c4
Aaron Decker <aaron@ard.ninja>
2016-10-28 19:34:27 -0400

    Update Dog.java

--- week10/week10/Dog.java
+++ week10/week10/Dog.java
@@ -3,17 +3,35 @@ package week10;
 public class Dog {
 
 	public String name;
+	public int age;
+	public int weight;
 
 	public Dog(String name) {
 		super();
 		this.name = name;
+	}
+	
+	public Dog(String name, int age, int weight) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.weight = weight;
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return "Dog [name=" + name + ", age=" + age + ", weight=" + weight + "]";
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + age;
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + weight;
 		return result;
 	}
 
@@ -26,12 +44,17 @@ public class Dog {
 		if (getClass() != obj.getClass())
 			return false;
 		Dog other = (Dog) obj;
+		if (age != other.age)
+			return false;
 		if (name == null) {
 			if (other.name != null)
 				return false;
 		} else if (!name.equals(other.name))
 			return false;
+		if (weight != other.weight)
+			return false;
 		return true;
 	}
+
 	
 }
