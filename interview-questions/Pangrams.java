5744d8a6560c7f55f395b1827decca62ab1dab37
Aaron Decker <aaron@ard.ninja>
2016-11-26 17:14:28 -0500

    Update Pangrams.java

--- interview-questions/Pangrams.java
+++ interview-questions/Pangrams.java
@@ -3,35 +3,30 @@ public class Pangrams {
 
 	public static void main(String[] args) throws Exception {
 
-		// String [] pangrams = new String [] {
-		// "The quick brown fox jumps over the lazy dog",
-		// "Pack my box with five dozen liquor jugs",
-		// "The five boxing wizards jump quickly"
-		// };
+		// is it a pangram?
+		String pangram = "The five boxing wizards jump quickly!";
 
-		String pangram = "The five boxing wizards jump quickly";
-
-		int[] ascii = new int[128];
-		// should find all of the letters btwn 97 + 122 inclusive
-		for (int i = 0; i < pangram.length(); i++) {
-			int test = pangram.toLowerCase().charAt(i);
-			if (test < 128) {
+		int [] ascii = new int[128];
+		
+		char [] chars = pangram.toLowerCase().toCharArray();
+		
+		for(int i = 0; i < chars.length; i++) {
+			int test = (int) chars[i];
+			if(test < 128) {
 				ascii[test] += 1;
 			}
 		}
-
-		boolean isPangram = true;
-		for (int i = 97; i <= 122; i++) {
-			if (ascii[i] == 0) {
-				System.out.println("not pangram");
-				isPangram = false;
-				break;
+		
+		System.out.println(Arrays.toString(ascii));
+		
+		for(int i = 97; i <= 122; i++) {
+			if(ascii[i] == 0) {
+				System.out.println("not a pangram!");
+				System.exit(1);
 			}
 		}
-
-		if (isPangram) {
-			System.out.println("is a pangram");
-		}
+		
+		System.out.println("Is a pangram!");
 	}
 
 }
